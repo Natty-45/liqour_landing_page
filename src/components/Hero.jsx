ab80ac1fa3985e9f0677891c4e81a9f5eb995810
Natty-45 <natty.ayalew.code@gmail.com>
2025-08-06 13:29:46 +0300

    feat: add About component with GSAP animations and integrate into App layout, adjust Hero component's end value for scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
 	.to(".arrow", { y: 100 }, 0);
 	
 	const startValue = isMobile ? "top 50%" : "center 60%";
-	const endValue = isMobile ? "120% top" : "bottom top";
+	const endValue = isMobile ? "173% top" : "bottom top";
 	
 	let tl = gsap.timeline({
 	 scrollTrigger: {
